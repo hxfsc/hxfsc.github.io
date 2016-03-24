9266e7d3bf9ac3935da0ba3ef1d3014966825173
胡师寰 <hxfsc2@gmail.com>
2016-03-25 00:44:52 +0800

    Site updated: 2016-03-25 00:44:52

--- ife/02-16/task.js
+++ ife/02-16/task.js
@@ -1,3 +1,26 @@
+    /**
+     * 简单的选择器
+     * 
+     */
+
+    function $( element ){
+        return document.getElementById( element );
+    }
+    /**
+     * 兼容操作
+     */
+    var EventUtil = {
+        addListerHandler: function(element, type, handle) {
+            if (element.addEventListener) {
+                element.addEventListener(type, handle, false);
+            } else if (element.attachEvent) {
+                element.attachEvent('on' + type, handle);
+            } else {
+                element['on' + type] = handle;
+            }
+        }
+    }
+
 /**
  * aqiData，存储用户输入的空气指数数据
  * 示例格式：
@@ -8,32 +31,7 @@
  */
 var aqiData = {};
 
-/**
- * 简单的选择器
- *
- */
 
-function $( element ){
-    return document.getElementById( element );
-}
-
-
-/**
- * 兼容操作
- */
-var EventUtil = {
-    addListerHandler: function(element, type, handle) {
-        if (element.addEventListener) {
-            element.addEventListener(type, handle, false);
-        } else if (element.attachEvent) {
-            element.attachEvent('on' + type, handle);
-        } else {
-            element['on' + type] = handle;
-        }
-    }
-
-
-}
 /**
  * { 城市名称:空气指数 }
  */
@@ -46,7 +44,7 @@ var _sortData = [];
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
-
+    
     var _apiData = {};
     var city = $("aqi-city-input").value;
     city = city.trim( city );
@@ -60,27 +58,27 @@ function addAqiData() {
         alert("空气质量指数必须为整数");
         return false;
     }
-
+   
     _apiData[ city ] = airApi;
     //排序
-
+    
     for( var i in _apiData ){
         _sortData.push( { api : _apiData[ i ], city : i } );
     }
-
-
-
+    
+    
+   
     _sortData.sort( function( a, b ){
         return b.api - a.api;
     });
-
-
+    
+    
     var apiData= {};
     delete apiDatal
     for( var i = 0; i < _sortData.length; i++ ){
         apiData[_sortData[i].city] = _sortData[i].api;
     }
-
+    
 
     sortData = apiData;
     return apiData;
@@ -92,18 +90,22 @@ function addAqiData() {
 function renderAqiList( data ) {
     if( data != false ){
         var _tableTemplate = [];
-        _tableTemplate.push("<tr class="thead"><td>城市</td><td>空气质量</td><td>操作</td></tr>");
+        _tableTemplate.push("<table><tr class="\"thead\""><td>城市</td><td>空气质量</td><td>操作</td></tr>");
         for (var i in data) {
-            _tableTemplate.push("<tr><td>" + i + "</td><td>" + data[i] + "</td><td class="del" onclick="delBtnHandle('"" +="" i="" "')="">删除</td></tr>");
+            _tableTemplate.push("<tr><td>" + i + "</td><td>" + data[i] + "</td><td class="del" onclick="\"delBtnHandle('"" +="" i="" "')\"="">删除</td></tr>");
         }
+        _tableTemplate.push("</table>");
         if( _tableTemplate.length > 1 ){
-            $("aqi-table").innerHTML = _tableTemplate.join("");
+            var _tableTemplateStr = _tableTemplate.join("");
+            $("aqi-table").innerHTML = _tableTemplateStr;
         } else{
             $("aqi-table").innerHTML = "";
         }
+        
+        
     }
-
-
+    
+   
 }
 
 /**
@@ -127,7 +129,7 @@ function delBtnHandle( city ) {
             _sortData.splice(i,1);
       }
   }
-  console.log( _sortData );
+  
   renderAqiList( sortData );
 }
 
@@ -135,13 +137,21 @@ function init() {
 
   // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
 
-
+  
   EventUtil.addListerHandler( $("add-btn"),"click", addBtnHandle );
-
+  
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
 
 }
 
 window.onload = function(){
+    if( !String.prototype.trim ){
+		String.prototype.trim =  function(){
+            return this.replace(/[(^\s+)(\s+$)]/g, "");
+        }
+	}
+
+
     init();
 }
+
